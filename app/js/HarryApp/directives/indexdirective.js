5b4fced63902606f584906145be4f36a82ce0b25
Larry Scroggins <lscroggins@blinkx.com>
2015-05-20 17:08:48 -0700

    cleaned up a few things. changed a little styling

--- app/js/HarryApp/directives/indexdirective.js
+++ app/js/HarryApp/directives/indexdirective.js
@@ -5,7 +5,6 @@ module.exports = function(app) {
 		return {
 			scope: {},
 			restrict: 'E',
-			// template: '<div class="card genericcard"><div class="cardoverlay"</div></div>'
 			link: function(scope, element, attrs) {
 				element.append('<div><div class="cardoverlay"></div>');
 				element.addClass('card genericcard');
@@ -25,18 +24,3 @@ module.exports = function(app) {
 		};
 	});
 };
-
-// $('.genericcard').each(function() {
-// 	var rNum = Math.random()*359;
-// 	var xScatter = Math.random()* bodyWidth;
-// 	var yScatter = Math.random() * bodyHeight;
-// 	var bgImage = (Math.floor(Math.random() * 21) + 1) + '.jpg';
-// 	$(this).css( {
-// 		"-webkit-transform": "rotate("+rNum+"2deg)",
-// 		"-moz-transform": "rotate("+rNum+"2deg)",
-// 		"top": yScatter + 'px',
-// 		"left": xScatter + 'px',
-// 		"background": 'url(/img/' + bgImage + ') no-repeat',
-// 		"background-size": "cover"
-// 	});
-// });
